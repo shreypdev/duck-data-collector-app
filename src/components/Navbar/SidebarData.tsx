e48687157f40cca91c6736f1be8a73dcdd02c57f
Shrey Patel <60997794+shreypdev@users.noreply.github.com>
2021-07-09 01:30:47 +0530

    Clean up and added scss, navigation and prettier support.

---
+++ src/components/Navbar/SidebarData.tsx
@@ -0,0 +1,10 @@
+import * as AiIcons from "react-icons/ai";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+];
