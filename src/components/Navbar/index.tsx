d4f2ef0964e31a2116f19effd14dac51a7f30ca2
Shrey Patel <60997794+shreypdev@users.noreply.github.com>
2021-07-10 13:18:15 +0530

    Added Navbar and did some clean up

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,45 +1,29 @@
-import React, { useState } from "react";
-import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
-import { Link } from "react-router-dom";
-import { SidebarData } from "./SidebarData";
+import React from "react";
 import "./Navbar.scss";
-import { IconContext } from "react-icons";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { FaUser, FaSignInAlt } from "react-icons/fa";
 
-export const Navbar: React.FC = () => {
-  const [sidebar, setSidebar] = useState(false);
-
-  const showSidebar = () => setSidebar(!sidebar);
-
+export const NavigationBar: React.FC = () => {
   return (
-    <>
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <div className="navbar">
-          <Link to="#" className="menu-bars">
-            <FaIcons.FaBars onClick={showSidebar} />
-          </Link>
-        </div>
-        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className="nav-menu-items" onClick={showSidebar}>
-            <li className="navbar-toggle">
-              <Link to="#" className="menu-bars">
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li>
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </IconContext.Provider>
-    </>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+      <Container>
+        <Navbar.Brand href="/">Duck Data Collector</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/add-new-data">Add New Data</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="/signup">
+              <FaUser /> Sign Up
+            </Nav.Link>
+            <Nav.Link href="/login">
+              <FaSignInAlt /> Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
